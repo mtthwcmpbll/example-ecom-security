c953504063fc52445568afab9037fd763ab38f56
Matthew Campbell <matt@moderne.io>
2025-12-31 17:15:51 -0500

    Baseline
    Co-authored-by: Moderne <team@moderne.io>

--- src/main/java/com/example/ecom/security/SecurityConfig.java
+++ src/main/java/com/example/ecom/security/SecurityConfig.java
@@ -1,16 +1,17 @@
 package com.example.ecom.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .authorizeRequests()
@@ -19,5 +20,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .oauth2ResourceServer()
                 .jwt();
+        return http.build();
     }
 }
